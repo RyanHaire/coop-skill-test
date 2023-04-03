8fe801005d7fa402db653b8700fc52b5d9811aa4
Ryan Haire <hairer@sheridancollege.ca>
2023-04-03 13:50:32 -0400

    Removed whitespace and unused imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './styles/App.scss';
 import Header from './components/Header';
 import Form from './components/Form';
@@ -6,11 +6,6 @@ import Form from './components/Form';
 
 function App() {
 
- 
-  
- 
-
-  
   return (
     <div className="bg-offwhite">
       <Header/>
